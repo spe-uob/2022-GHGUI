a58e9fa226407cc29e2e18537a542da333ab482b
fang <98462105+su21085@users.noreply.github.com>
2023-02-02 13:01:38 -0800

    login

---
+++ src/main/java/uk/ac/bristol/FileOperator.java
@@ -0,0 +1,72 @@
+package uk.ac.bristol;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FileOperator {
+
+
+
+
+
+
+    public static List<User> userList;
+
+
+    public static void write(User user) {
+        File fileDest = new File("data.txt");
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter(fileDest, true));
+            bw.append(user.getUsername() + "/" + user.getPassword() + "\n");
+            bw.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != bw) {
+                    bw.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+    }
+
+
+    public static void read() {
+        userList = new ArrayList<>();
+        File src = new File("data.txt");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(src));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String[] strList = line.split("/");
+
+
+                String userName = strList[0];
+                String password = strList[1];
+                User user = new User(userName, password);
+                userList.add(user);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != reader) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
