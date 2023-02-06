faa4fbe833e269be27bb7f175b2098a38dfb16dc
fang <98462105+su21085@users.noreply.github.com>
2023-02-06 15:48:22 -0800

    Login and Register

--- src/main/java/uk/ac/bristol/LoginAndRegister.java
+++ src/main/java/uk/ac/bristol/LoginAndRegister.java
@@ -1,13 +1,15 @@
 package uk.ac.bristol;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Optional;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Optional;
+
 
 public class LoginAndRegister extends Application {
   private static Stage stage;
@@ -41,22 +43,23 @@ public class LoginAndRegister extends Application {
     lblAccount.setLayoutY(87);
     panel.getChildren().add(lblAccount);
 
+
     Button btnLogin = new Button("login");
     btnLogin.setLayoutX(135);
     btnLogin.setLayoutY(207);
     btnLogin.setPrefSize(116, 24);
     panel.getChildren().add(btnLogin);
-    btnLogin.setOnAction(
-        event -> {
-          try {
-            userLogin();
-          } catch (URISyntaxException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        });
+    btnLogin.setOnAction(event -> {
+      try {
+        userLogin();
+      } catch (URISyntaxException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
+
 
     lblPassword = new Label("password:");
     lblPassword.setLayoutX(135);
@@ -69,10 +72,9 @@ public class LoginAndRegister extends Application {
     panel.getChildren().add(user_password);
 
     Button registerButton = new Button("register");
-    registerButton.setOnAction(
-        event -> {
-          userRigister();
-        });
+    registerButton.setOnAction(event -> {
+      userRigister();
+    });
 
     registerButton.setLayoutX(300);
     registerButton.setLayoutY(207);
@@ -81,51 +83,65 @@ public class LoginAndRegister extends Application {
     Scene scene = new Scene(contentPane);
     stage.setScene(scene);
     stage.show();
+
+
   }
+
+
+
+
+
+
+
+
+
 
   private void userLogin() throws URISyntaxException, IOException {
 
-    // check input
+
+    //check input
     if (checkInput() == -1) {
       return;
     }
     // login check
     UserLogin(this.user_name.getText(), this.user_password.getText());
+
   }
 
-  /** 客户注册页面校验 */
+  /**
+   * customer registration page verification
+   */
   protected void userRigister() {
 
-    // check input
+    //check input
     if (checkInput() == -1) {
       return;
     }
     ;
     // register check
     UserRigister(this.user_name.getText(), this.user_password.getText());
+
   }
 
-  /** check input */
+  /**
+   * check input
+   */
   public int checkInput() {
     String userName = this.user_name.getText();
     String password = new String(this.user_password.getText());
     if (userName.trim().equals("")) {
-      new Alert(
-              Alert.AlertType.NONE, "account cannot be empty", new ButtonType[] {ButtonType.CLOSE})
-          .show();
+      new Alert(Alert.AlertType.NONE, "account cannot be empty", new ButtonType[]{ButtonType.CLOSE}).show();
       return -1;
     }
     if ((password.trim()).equals("")) {
-      new Alert(
-              Alert.AlertType.NONE, "password cannot be empty", new ButtonType[] {ButtonType.CLOSE})
-          .show();
+      new Alert(Alert.AlertType.NONE, "password cannot be empty", new ButtonType[]{ButtonType.CLOSE}).show();
       return -1;
     }
     return 0;
   }
 
   /**
-   * check login
+   * check  login
    *
    * @param userName
    * @param password
@@ -137,22 +153,18 @@ public class LoginAndRegister extends Application {
 
     for (int i = 0; i < FileOperator.userList.size(); i++) {
       if (FileOperator.userList.get(i).getUsername().trim().equals(userName)
-          && FileOperator.userList.get(i).getPassword().trim().equals(password)) {
+              && FileOperator.userList.get(i).getPassword().trim().equals(password)) {
         User = FileOperator.userList.get(i);
-        // close stage
-        // open new window
-        new Alert(Alert.AlertType.NONE, "login success", new ButtonType[] {ButtonType.CLOSE})
-            .show();
-        App app = new App();
+        //close stage
+        //open new window
+        new Alert(Alert.AlertType.NONE, "login success", new ButtonType[]{ButtonType.CLOSE}).show();
+        App app =new App();
         app.loadMain(stage);
       }
+
     }
     if (User == null) {
-      new Alert(
-              Alert.AlertType.NONE,
-              "account or password is incorrect!",
-              new ButtonType[] {ButtonType.CLOSE})
-          .show();
+      new Alert(Alert.AlertType.NONE, "account or password is incorrect!", new ButtonType[]{ButtonType.CLOSE}).show();
       user_name.setText("");
       user_password.setText("");
     }
@@ -166,29 +178,21 @@ public class LoginAndRegister extends Application {
    */
   private void UserRigister(String userName, String password) {
     FileOperator.read();
-    // 查找是否有此账号
+    //查找是否有此账号
     User User = null;
     for (int i = 0; i < FileOperator.userList.size(); i++) {
       if (FileOperator.userList.get(i).getUsername().trim().equals(userName)
-          && FileOperator.userList.get(i).getPassword().trim().equals(password)) {
+              && FileOperator.userList.get(i).getPassword().trim().equals(password)) {
         User = FileOperator.userList.get(i);
-        new Alert(
-                Alert.AlertType.NONE,
-                "user already exists. Please re-register",
-                new ButtonType[] {ButtonType.CLOSE})
-            .show();
+        new Alert(Alert.AlertType.NONE, "user already exists. Please re-register", new ButtonType[]{ButtonType.CLOSE}).show();
         user_name.setText("");
         user_password.setText("");
       }
     }
-    // 没有则录入，显示提示信息
+    //if not,log in.Display prompt information
     if (User == null) {
       FileOperator.write(new User(userName, password));
-      new Alert(
-              Alert.AlertType.NONE,
-              "register successfully, please login",
-              new ButtonType[] {ButtonType.CLOSE})
-          .show();
+      new Alert(Alert.AlertType.NONE, "register successfully, please login", new ButtonType[]{ButtonType.CLOSE}).show();
       user_name.setText("");
       user_password.setText("");
     }
@@ -203,15 +207,9 @@ public class LoginAndRegister extends Application {
    * @param message
    * @return boolean
    */
-  public boolean informationDialog(
-      Alert.AlertType alterType, String title, String header, String message) {
-    // You can use a preset button section or you can create a new one like this
-    Alert alert =
-        new Alert(
-            alterType,
-            message,
-            new ButtonType("cancle", ButtonBar.ButtonData.CANCEL_CLOSE),
-            new ButtonType("yes", ButtonBar.ButtonData.YES));
+  public boolean informationDialog(Alert.AlertType alterType, String title, String header, String message) {
+    //You can use a preset button section or you can create a new one like this
+    Alert alert = new Alert(alterType, message, new ButtonType("cancle", ButtonBar.ButtonData.CANCEL_CLOSE), new ButtonType("yes", ButtonBar.ButtonData.YES));
     // // Set the title of the dialog box
     alert.setTitle(title);
     alert.setHeaderText(header);
@@ -220,4 +218,5 @@ public class LoginAndRegister extends Application {
     // Returns the result of the click, or true if "OK" is clicked
     return buttonType.get().getButtonData().equals(ButtonBar.ButtonData.YES);
   }
+
 }
