33516d7f4b517d1090333277a96bf14b3d9505e6
fang <98462105+su21085@users.noreply.github.com>
2023-02-06 15:48:50 -0800

    Controllers

--- src/main/java/uk/ac/bristol/controllers/MainController.java
+++ src/main/java/uk/ac/bristol/controllers/MainController.java
@@ -7,10 +7,13 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.RepositoryBuilder;
+import uk.ac.bristol.LoginAndRegister;
 import uk.ac.bristol.controllers.factories.TabControllerFactory;
 import uk.ac.bristol.util.errors.ErrorHandler;
+import javafx.event.ActionEvent;
 
 /** The FXML controller for the main window. */
 public class MainController {
@@ -31,7 +34,7 @@ public class MainController {
     }
 
     final RepositoryBuilder repositoryBuilder =
-        new RepositoryBuilder().findGitDir(selectedDirectory);
+            new RepositoryBuilder().findGitDir(selectedDirectory);
     final File gitDirectory = repositoryBuilder.getGitDir();
 
     if (gitDirectory == null) {
@@ -41,9 +44,22 @@ public class MainController {
 
     final Tab tab = new Tab(gitDirectory.getParentFile().getName());
     ErrorHandler.deferredCatch(
-        () ->
-            tab.setContent(
-                TabControllerFactory.build(new Git(repositoryBuilder.readEnvironment().build()))));
+            () ->
+                    tab.setContent(
+                            TabControllerFactory.build(new Git(repositoryBuilder.readEnvironment().build()))));
     tabs.getTabs().add(tab);
+  }
+
+
+  @FXML
+  void loginClick(ActionEvent event) throws Exception {
+    LoginAndRegister loginAndRegister = new LoginAndRegister();
+    loginAndRegister.start(this.stage);
+
+  }
+
+  private Stage stage;
+  public void setStage(Stage primaryStage) {
+    this.stage = primaryStage;
   }
 }
