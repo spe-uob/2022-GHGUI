df4b27ac84e899b96a470546324b15c85c199c65
Daniel D <daniel.mi.do@gmail.com>
2023-02-07 15:18:21 +0000

    removed the project that was created as a subfolder

--- 2022-GHGUI-dev/src/main/java/uk/ac/bristol/util/errors/CheckedProcedure.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.bristol.util.errors;
-
-/** Represents a Procedure that may throw an Exception. */
-@FunctionalInterface
-public interface CheckedProcedure {
-  /** {@inheritDoc} */
-  void run() throws Exception;
-}
