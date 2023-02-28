d82e11d927179f4d79724b45cddc49eb68eaf129
Foxinatel <nathanielma5on@yahoo.com>
2023-02-28 14:33:42 +0000

    Use varargs for EventBus refresh method

--- src/main/java/uk/ac/bristol/controllers/events/EventBus.java
+++ src/main/java/uk/ac/bristol/controllers/events/EventBus.java
@@ -8,17 +8,22 @@ import java.util.Map;
 public class EventBus {
   Map<Class<?>, List<Object>> registered = new HashMap<Class<?>, List<Object>>();
 
-  public void register(Object obj) {
+  public final void register(Object obj) {
     var list = registered.getOrDefault(obj.getClass(), new ArrayList<Object>());
     list.add(obj);
     registered.put(obj.getClass(), list);
   }
 
-  public void refresh(Class<? extends Refreshable> T) {
-    var list = registered.get(T);
-    for (Object elem : list) {
-      var obj = T.cast(elem);
-      obj.refresh();
+  // Might leak data onto the heap...
+  // But lets be honest, we've got far worse memory leaks to deal with...
+  @SafeVarargs
+  public final void refresh(Class<? extends Refreshable>... Ts) {
+    for (var T : Ts) {
+      var list = registered.get(T);
+      for (Object elem : list) {
+        var obj = T.cast(elem);
+        obj.refresh();
+      }
     }
   }
 }
