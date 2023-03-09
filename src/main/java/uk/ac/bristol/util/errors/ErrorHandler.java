ecdad329f90bb611106dadc1204f2fcefd1c6199
Foxinatel <nathanielma5on@yahoo.com>
2023-03-09 14:22:21 +0000

    Completely refactor the Error Handler to use Tasks

--- src/main/java/uk/ac/bristol/util/errors/ErrorHandler.java
+++ src/main/java/uk/ac/bristol/util/errors/ErrorHandler.java
@@ -1,7 +1,8 @@
 package uk.ac.bristol.util.errors;
 
-import java.util.Optional;
+import java.util.function.Consumer;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,37 +11,67 @@ import lombok.extern.slf4j.Slf4j;
 @UtilityClass
 @Slf4j
 public class ErrorHandler {
+  /** A class for building tasks. */
+  // CHECKSTYLE:IGNORE HideUtilityClassConstructorCheck 1
+  @UtilityClass
+  private class ThrowingTaskBuilder {
+    /**
+     * Create a new task from a function.
+     *
+     * @param f The function to build this task from
+     * @param <T> The type returned by this function
+     * @return The task made from this function
+     */
+    protected <T> Task<T> build(final CheckedSupplier<T> f) {
+      final Task<T> task =
+          new Task<T>() {
+            @Override
+            protected T call() throws Exception {
+              return f.get();
+            }
+          };
+      task.setOnFailed(__ -> ErrorHandler.handle(task.getException()));
+      return task;
+    }
+  }
+
+  /**
+   * Simple wrapper for helping to generate Task<Void>.
+   *
+   * @param f The function to wrap
+   * @return A wrapped function that will return null on success
+   */
+  private static CheckedSupplier<Void> nullOnSuccess(final CheckedProcedure f) {
+    return () -> {
+      f.run();
+      return null;
+    };
+  }
+
   /**
    * Accepts a function that may throw an Exception, returning the value on success and gracefully
    * handling Exceptions if thrown.
    *
    * @param f The function to call
    * @param <T> The return type of the input function
-   * @return The result of the function if no Exception was thrown, otherwise null
+   * @return A task representing this function
    */
-  public static <T> Optional<T> mightFail(final CheckedSupplier<T> f) {
-    try {
-      return Optional.of(f.get());
-    } catch (Exception ex) {
-      handle(ex);
-      return Optional.empty();
-    }
+  public static <T> Task<T> mightFail(final CheckedSupplier<T> f) {
+    final Task<T> task = ThrowingTaskBuilder.build(f);
+    task.run();
+    return task;
   }
 
   /**
    * Accepts a procedure that may throw an Exception, gracefully handling Exceptions if thrown.
    *
    * @param f The function to call
+   * @return A task representing this object
    */
-  public static void mightFail(final CheckedProcedure f) {
-    Platform.runLater(
-        () -> {
-          try {
-            f.run();
-          } catch (Exception ex) {
-            handle(ex);
-          }
-        });
+  public static Task<Void> mightFail(final CheckedProcedure f) {
+    final Task<Void> task = ThrowingTaskBuilder.build(nullOnSuccess(f));
+    task.run();
+    return task;
   }
 
   /**
@@ -50,73 +81,37 @@ public class ErrorHandler {
    * @param <T> The type produced by the Supplier and used by the Consumer
    * @param s The Supplier for the value
    * @param c The Consumer to apply the value to
+   * @return The task corresponding to this series of operations
    */
-  public static <T> void tryWith(final CheckedSupplier<T> s, final CheckedConsumer<T> c) {
-    Platform.runLater(
-        () -> {
-          try {
-            c.accept(s.get());
-          } catch (Exception ex) {
-            handle(ex);
-          }
-        });
+  public static <T> Task<Void> tryWith(final CheckedSupplier<T> s, final CheckedConsumer<T> c) {
+
+    final Task<Void> task;
+    if (c instanceof Consumer<?>) {
+      final Consumer<T> cons = (Consumer<T>) c;
+      task =
+          ThrowingTaskBuilder.build(
+              () -> {
+                final T res = s.get();
+                Platform.runLater(() -> cons.accept(res));
+                return null;
+              });
+    } else {
+      task = ThrowingTaskBuilder.build(nullOnSuccess(() -> c.accept(s.get())));
+    }
+    task.run();
+    return task;
   }
-
-  // /**
-  //  * Accepts a function that may throw an Exception, returning the value on success and
-  // gracefully
-  //  * handling Exceptions if thrown.
-  //  *
-  //  * @param f The function to call
-  //  * @param msg An error message to log if an Exception occurs
-  //  * @param <T> The return type of the input function
-  //  * @return The result of the function if no Exception was thrown, otherwise null
-  //  */
-  // public static <T> T deferredCatch(final CheckedSupplier<T> f, final String msg) {
-  //   try {
-  //     return f.get();
-  //   } catch (Exception ex) {
-  //     handle(ex, msg);
-  //     return null;
-  //   }
-  // }
-
-  // /**
-  //  * Accepts a procedure that may throw an Exception, gracefully handling Exceptions if thrown.
-  //  *
-  //  * @param f The function to call
-  //  * @param msg An error message to log if an Exception occurs
-  //  */
-  // public static void deferredCatch(final CheckedProcedure f, final String msg) {
-  //   try {
-  //     f.run();
-  //   } catch (Exception ex) {
-  //     handle(ex, msg);
-  //   }
-  // }
 
   /**
-   * Grafefully handles errors, logging them with Slf4j and producing a visiable alert for the user.
+   * Gracefully handles errors, logging them with Slf4j and producing a visiable alert for the user.
    *
-   * @param ex An Exception to be handled
+   * @param t A Throwable to be handled
    */
-  public static void handle(final Exception ex) {
-    AlertBuilder.fromException(ex).show();
-    log.error(ex.getLocalizedMessage(), ex);
+  public static void handle(final Throwable t) {
+    if (t instanceof Exception ex) {
+      AlertBuilder.fromException(ex).show();
+      log.error(ex.getLocalizedMessage(), ex);
+    }
+    // TODO: else?
   }
-
-  // /**
-  //  * Grafefully handles errors, logging them with Slf4j and producing a visiable alert for the
-  // user.
-  //  *
-  //  * @param thrown The Exception to handle
-  //  * @param msg An error message to log with the Exception
-  //  */
-  // public static void handle(final Throwable thrown, final String msg) {
-  //   if (thrown instanceof Exception ex) {
-  //     AlertBuilder.fromException(ex).showAndWait();
-  //     log.error(msg, ex);
-  //   } else {
-  //   }
-  // }
 }
