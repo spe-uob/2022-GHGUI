6d4f7e9da5daa666ac1f1b617e2305699dddab54
Foxinatel <nathanielma5on@yahoo.com>
2023-03-22 20:43:27 +0000

    New logic for generating entries in the combo boxes

--- src/main/java/uk/ac/bristol/controllers/PullController.java
+++ src/main/java/uk/ac/bristol/controllers/PullController.java
@@ -1,21 +1,18 @@
 package uk.ac.bristol.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TitledPane;
 import javafx.stage.Stage;
-import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.transport.RemoteConfig;
 import uk.ac.bristol.controllers.events.EventBus;
@@ -23,8 +20,7 @@ import uk.ac.bristol.util.GitInfo;
 import uk.ac.bristol.util.errors.ErrorHandler;
 
 /** The FXML class to handle the Commit pop-up window. */
-@Slf4j
-public class PullController implements Initializable {
+public class PullController {
 
   /** The event bus used for refresh events for this tab. */
   private EventBus eventBus;
@@ -35,11 +31,14 @@ public class PullController implements Initializable {
   /** The root pane for this controller. */
   @FXML private TitledPane root;
 
-  /** Source branch name selection. */
-  @FXML private ComboBox<String> sourceComboBox;
-
   /** Remote name selection. */
-  @FXML private ComboBox<String> remoteComboBox;
+  @FXML private ComboBox<String> remote;
+
+  /** Source branch name selection. */
+  @FXML private ComboBox<String> remoteBranch;
+
+  /** Source branch name selection. */
+  @FXML private ComboBox<String> targetBranch;
 
   /**
    * Constructor for the CommitController. Registers obect to the EventBus.
@@ -67,19 +66,16 @@ public class PullController implements Initializable {
    */
   @FXML
   void pull(final ActionEvent event) {
-    final String branch =
-        sourceComboBox.getValue() != null
-            ? sourceComboBox.getValue()
-            : sourceComboBox.getPromptText();
+    final String branchName =
+        remoteBranch.getValue() != null ? remoteBranch.getValue() : remoteBranch.getPromptText();
 
-    final String remote =
-        remoteComboBox.getValue() != null
-            ? remoteComboBox.getValue()
-            : remoteComboBox.getPromptText();
+    final String remoteName =
+        remote.getValue() != null ? remote.getValue() : remote.getPromptText();
 
     final PullCommand pullCommand = gitInfo.command(Git::pull);
+    // Haven't tested this yet, but I'm absolutely certain it's just gonna print out a pointer
     ErrorHandler.tryWith(
-        pullCommand.setRemote(remote).setRemoteBranchName(branch)::call,
+        pullCommand.setRemote(remoteName).setRemoteBranchName(branchName)::call,
         pullResult -> {
           final Alert alert = new Alert(Alert.AlertType.INFORMATION);
           alert.setResizable(true);
@@ -91,45 +87,50 @@ public class PullController implements Initializable {
         });
   }
 
-  /** {@inheritDoc} */
-  @Override
-  public void initialize(final URL location, final ResourceBundle resources) {
-    initComBox();
-  }
-
-  /** set comBox value. */
-  public void initComBox() {
-    // 1.set remotesComBox option
+  /** Populate remotes. */
+  @FXML
+  private void populateRemotes() {
     ErrorHandler.tryWith(
         gitInfo.command(Git::remoteList)::call,
         remotes -> {
           final var items = remotes.stream().map(RemoteConfig::getName).toList();
-          remoteComboBox.setItems(FXCollections.observableList(items));
+          remote.setItems(FXCollections.observableList(items));
         });
+  }
 
-    // 2.set branchComBox option
+  /** Populate remote branches. */
+  @FXML
+  private void populateRemoteBranches() {
+    final String remoteName =
+        remote.getValue() != null ? remote.getValue() : remote.getPromptText();
     ErrorHandler.tryWith(
         gitInfo.command(Git::branchList).setListMode(ListBranchCommand.ListMode.REMOTE)::call,
         refList -> {
-          final ObservableList<String> remoteBranchOptions = FXCollections.observableArrayList();
+          final ObservableList<String> branchOptions = FXCollections.observableArrayList();
           for (Ref ref : refList) {
-            final String refName = ref.getName();
-            if (refName.startsWith("refs/remotes/origin/")) {
-              final String branchName = refName.replace("refs/remotes/origin/", "");
+            final String refName = ref.getName().substring(Constants.R_REMOTES.length());
+            if (refName.startsWith(remoteName)) {
+              final String branchName = refName.substring(remoteName.length() + 1);
               if (!branchName.equals("HEAD")) {
-                remoteBranchOptions.add(branchName);
+                branchOptions.add(branchName);
               }
             }
           }
-          sourceComboBox.setItems(remoteBranchOptions);
+          remoteBranch.setItems(branchOptions);
         });
+  }
 
+  /** Populate local branches. */
+  @FXML
+  private void populateTargetBranches() {
     ErrorHandler.tryWith(
         gitInfo.command(Git::branchList)::call,
-        branches -> {
-          if (branches.size() != 0) {
-            sourceComboBox.setPromptText(branches.get(0).getName().replace("refs/heads/", ""));
-          }
+        refList -> {
+          final var branches =
+              refList.stream()
+                  .map(ref -> ref.getName().substring(Constants.R_HEADS.length()))
+                  .toList();
+          targetBranch.setItems(FXCollections.observableList(branches));
         });
   }
 }
