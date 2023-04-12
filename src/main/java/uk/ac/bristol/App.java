a8a57bea0661a4eb3b52605f6cd435fcb56ca2a9
Michael <rs21359@bristol.ac.uk>
2023-04-12 04:17:02 +0100

    Implement a button on the top menu bar.

--- src/main/java/uk/ac/bristol/App.java
+++ src/main/java/uk/ac/bristol/App.java
@@ -4,9 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import lombok.experimental.UtilityClass;
 import uk.ac.bristol.controllers.Controller;
+import uk.ac.bristol.controllers.GitCleanController;
 
 import java.io.IOException;
 
@@ -85,3 +91,42 @@ public class App extends Application {
     launch(args);
   }
 }
+
+
+class Main extends Application {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    VBox root = new VBox();
+    MenuBar menuBar = new MenuBar();
+    Menu gitMenu = new Menu("Git");
+    MenuItem cleanMenuItem = new MenuItem("Clean");
+    cleanMenuItem.setOnAction(event -> showGitCleanDialog(primaryStage));
+    gitMenu.getItems().add(cleanMenuItem);
+    menuBar.getMenus().add(gitMenu);
+    root.getChildren().addAll(menuBar);
+    Scene scene = new Scene(root, 800, 600);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+  private void showGitCleanDialog(Stage parentStage) {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/GitCleanDialog.fxml"));
+      Parent root = loader.load();
+      GitCleanController controller = loader.getController();
+      Stage dialogStage = new Stage();
+      dialogStage.initOwner(parentStage);
+      dialogStage.initModality(Modality.WINDOW_MODAL);
+      dialogStage.setScene(new Scene(root));
+      controller.setDialogStage(dialogStage);
+      dialogStage.showAndWait();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
+
