f98d462d6499d876035d67f8e39fdfc9a2857456
su21085 <su21085@bristol.ac.uk>
2023-04-14 23:42:15 +0800

    add MergeControllerFactory

---
+++ src/main/java/uk/ac/bristol/controllers/factories/MergeControllerFactory.java
@@ -0,0 +1,24 @@
+package uk.ac.bristol.controllers.factories;
+
+import uk.ac.bristol.controllers.PullController;
+import uk.ac.bristol.controllers.events.EventBus;
+import uk.ac.bristol.util.GitInfo;
+
+/** A class for building MergeControllerFactory. */
+public final class MergeControllerFactory extends ControllerFactory {
+  /**
+   * Construct a new MergeControllerFactory.
+   *
+   * @param eventBus The EventBus shared by this tab
+   * @param gitInfo Information about the git repo for this tab
+   */
+  public MergeControllerFactory(final EventBus eventBus, final GitInfo gitInfo) {
+    getLoader().setControllerFactory(__ -> new PullController(eventBus, gitInfo));
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  String getResourceDir() {
+    return "fxml-resources/merge.fxml";
+  }
+}
