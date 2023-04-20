59088b41b03cb7c9e2135dfe4e8d49cd29bce8b4
Michael <rs21359@bristol.ac.uk>
2023-04-20 17:04:50 +0100

    Implement a button on the top menu bar using git revert.

---
+++ src/main/java/uk/ac/bristol/RevertMain.java
@@ -0,0 +1,23 @@
+package uk.ac.bristol;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RevertMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Revert.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root,320,240);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Git Application");
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
