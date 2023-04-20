8c8e2eafeaa2ee32fbf672ef171ccdb3e09f8d0b
Michael <rs21359@bristol.ac.uk>
2023-04-20 17:28:25 +0100

    Implement a button on the top menu bar using git revert.

--- src/main/java/uk/ac/bristol/controllers/RevertController.java
+++ src/main/java/uk/ac/bristol/controllers/RevertController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Optional;
 
-public class GitRevertController {
+public class RevertController {
 
     @FXML
     private void Revert(ActionEvent event) {
