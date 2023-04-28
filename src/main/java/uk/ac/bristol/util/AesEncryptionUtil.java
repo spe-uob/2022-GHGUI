834cb4a1e327219dc93fd69136628d12cb5d86d4
su21085 <su21085@bristol.ac.uk>
2023-04-28 23:42:04 +0800

    add encrypt void

--- src/main/java/uk/ac/bristol/util/AesEncryptionUtil.java
+++ src/main/java/uk/ac/bristol/util/AesEncryptionUtil.java
@@ -1,12 +1,37 @@
 package uk.ac.bristol.util;
 
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import lombok.experimental.UtilityClass;
+
 /**
  * The AesEncryptionUtil class is a Java utility class that provides methods for encrypting and
  * decrypting text using the AES encryption algorithm.
  */
+@UtilityClass
 public class AesEncryptionUtil {
   /** The encryption algorithm used. */
   private static final String ALGORITHM = "AES";
   /** The mode and padding used. */
   private static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
+
+  /**
+   * Encryption method.
+   *
+   * @param plainText your want convert text
+   * @param key your Encryption Key
+   * @return encrypt
+   * @throws Exception exception
+   */
+  public static String encrypt(final String plainText, final String key) throws Exception {
+    // Create a SecretKeySpec object from the key
+    final SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), ALGORITHM);
+    // Create a Cipher object using the specified transformation
+    final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec); // Initialize the cipher with the key and the mode
+    final byte[] encrypted = cipher.doFinal(plainText.getBytes()); // Encrypt the plaintext
+    // Encode the encrypted data as a Base64 string
+    return Base64.getEncoder().encodeToString(encrypted);
+  }
 }
