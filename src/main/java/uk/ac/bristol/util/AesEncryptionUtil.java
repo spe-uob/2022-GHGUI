c5065c1187e86041c89235a2bbc9c29da66efe0a
su21085 <su21085@bristol.ac.uk>
2023-04-28 23:36:30 +0800

    add AesEncryptionUtil and fill some AesEncryptionUtil

---
+++ src/main/java/uk/ac/bristol/util/AesEncryptionUtil.java
@@ -0,0 +1,16 @@
+package uk.ac.bristol.util;
+
+/**
+ * The AesEncryptionUtil class is a Java utility class that provides methods for encrypting
+ * and decrypting text using the AES encryption algorithm.
+ */
+public class AesEncryptionUtil {
+    /**
+     *  The encryption algorithm used
+     */
+    private static final String ALGORITHM = "AES";
+    /**
+     * The mode and padding used
+     */
+    private static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
+}
