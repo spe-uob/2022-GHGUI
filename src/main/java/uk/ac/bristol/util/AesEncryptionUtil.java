651d602fa92cd2bc85282fb9556e02df1dafed15
su21085 <su21085@bristol.ac.uk>
2023-04-28 23:48:13 +0800

    add decrypt void

--- src/main/java/uk/ac/bristol/util/AesEncryptionUtil.java
+++ src/main/java/uk/ac/bristol/util/AesEncryptionUtil.java
@@ -22,7 +22,7 @@ public class AesEncryptionUtil {
     /**
      * Encryption method
      * @param plainText  your want convert text
-     * @param key  your Encryption  Key
+     * @param key  your   Key
      * @return   encrypt
      * @throws Exception exception
      */
@@ -32,6 +32,24 @@ public class AesEncryptionUtil {
         cipher.init(Cipher.ENCRYPT_MODE, keySpec); // Initialize the cipher with the key and the mode
         byte[] encrypted = cipher.doFinal(plainText.getBytes()); // Encrypt the plaintext
         return Base64.getEncoder().encodeToString(encrypted); // Encode the encrypted data as a Base64 string
+    }
+
+
+
+
+    /**
+     * Decryption method
+     * @param encryptedText  your encryption text
+     * @param key your   Key
+     * @return decrypt text
+     * @throws Exception exception
+     */
+    public static String decrypt(String encryptedText, String key) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), ALGORITHM); // Create a SecretKeySpec object from the key
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION); // Create a Cipher object using the specified transformation
+        cipher.init(Cipher.DECRYPT_MODE, keySpec); // Initialize the cipher with the key and the mode
+        byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedText)); // Decrypt the encrypted text
+        return new String(decrypted); // Return the decrypted plaintext as a string
     }
 
 
