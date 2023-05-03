73eafc589228e1adc6fd4bcbef81e299c8235dcf
Foxinatel <nathanielma5on@yahoo.com>
2023-05-03 05:32:15 +0100

    Clean up some now-dead code

--- src/main/java/uk/ac/bristol/util/auth/AesEncryptionUtil.java
+++ src/main/java/uk/ac/bristol/util/auth/AesEncryptionUtil.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.security.SecureRandom;
-import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
@@ -98,43 +97,5 @@ public class AesEncryptionUtil {
       e.printStackTrace();
       // TODO: HANDLE
     }
-  }
-
-  /**
-   * Encryption method.
-   *
-   * @param plainText your want convert text
-   * @param key your Encryption Key
-   * @return encrypt
-   * @throws Exception exception
-   */
-  public static String encrypt(final String plainText, final String key) throws Exception {
-    // Create a SecretKeySpec object from the key
-    final SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), ALGORITHM);
-    // Create a Cipher object using the specified transformation
-    final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-    cipher.init(Cipher.ENCRYPT_MODE, keySpec); // Initialize the cipher with the key and the mode
-    final byte[] encrypted = cipher.doFinal(plainText.getBytes()); // Encrypt the plaintext
-    // Encode the encrypted data as a Base64 string
-    return Base64.getEncoder().encodeToString(encrypted);
-  }
-
-  /**
-   * Decryption method.
-   *
-   * @param encryptedText your encryption text
-   * @param key your Key
-   * @return decrypt text
-   * @throws Exception exception
-   */
-  public static String decrypt(final String encryptedText, final String key) throws Exception {
-    // Create a SecretKeySpec object from the key
-    final SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), ALGORITHM);
-    // Create a Cipher object using the specified transformation
-    final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-    cipher.init(Cipher.DECRYPT_MODE, keySpec); // Initialize the cipher with the key and the mode
-    // Decrypt the encrypted text
-    final byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
-    return new String(decrypted); // Return the decrypted plaintext as a string
   }
 }
